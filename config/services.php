2189cc76e71d0e061f08bfa8e57ee651e9031247
deepakpatel1999 <deeppatelg1122@gmai.com>
2022-11-25 19:47:43 +0530

    google login without packeg

---
+++ config/services.php
@@ -0,0 +1,39 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Third Party Services
+    |--------------------------------------------------------------------------
+    |
+    | This file is for storing the credentials for third party services such
+    | as Mailgun, Postmark, AWS and more. This file provides the de facto
+    | location for this type of information, allowing packages to have
+    | a conventional file to locate the various service credentials.
+    |
+    */
+
+    'mailgun' => [
+        'domain' => env('MAILGUN_DOMAIN'),
+        'secret' => env('MAILGUN_SECRET'),
+        'endpoint' => env('MAILGUN_ENDPOINT', 'api.mailgun.net'),
+        'scheme' => 'https',
+    ],
+
+    'postmark' => [
+        'token' => env('POSTMARK_TOKEN'),
+    ],
+
+    'ses' => [
+        'key' => env('AWS_ACCESS_KEY_ID'),
+        'secret' => env('AWS_SECRET_ACCESS_KEY'),
+        'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
+    ],
+    'google' => [
+        'client_id' => '536783955512-sl3v5hhmabh3liaslb1oaha8gehf2mgu.apps.googleusercontent.com',
+        'client_secret' => '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',
+        'redirect' => 'http://localhost:8000/auth/google/callback',
+        'app_name' =>'googlelogin',
+    ],
+];
